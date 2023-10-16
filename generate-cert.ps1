e81391a087b268298756968f93bd556f9ff00870
Ikem Krueger <ikem.krueger@gmail.com>
2023-10-16 23:37:32 +0200

    Implement config file support for generate-cert.ps1

--- generate-cert.ps1
+++ generate-cert.ps1
@@ -1,13 +1,19 @@
+$ConfigFile = Get-Content "Certificate.ini" | ConvertFrom-StringData
+
+$CommonName = $ConfigFile.CommonName
+$Organization = $ConfigFile.Organization
+$Country = $ConfigFile.Country
+$FriendlyName = $ConfigFile.FriendlyName
+
+$CertPassword = Read-Host 'Enter Export Password' -AsSecureString
+
 $cert = New-SelfSignedCertificate `
     -Type CodeSigningCert `
-    -Subject "CN=Cloudflight Operate Code Signing (INTERNAL), O=Cloudflight GmbH, C=DE" `
-    -FriendlyName "Cloudflight Operate Code Signing (INTERNAL ONLY)" `
+    -Subject "CN=$CommonName, O=$Organization, C=$Country" `
+    -FriendlyName "$FriendlyName" `
     -CertStoreLocation "Cert:\CurrentUser\My" `
     -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
-if(Test-Path "passphrase.secret") {
-    $CertPassword = ConvertTo-SecureString -String (Get-Content ".\passphrase.secret").Trim() -AsPlainText -Force
-} else {
-    $CertPassword = Read-Host 'Enter Export Password' -AsSecureString
-}
+
+Write-Host "Export Thumbprint: $($cert.Thumbprint)"
 
 Export-PfxCertificate -Cert "cert:\CurrentUser\My\$($cert.Thumbprint)" -FilePath "Certificate.pfx" -Password $CertPassword
